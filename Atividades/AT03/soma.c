a20fd1fea45363aa7a0b580eb9791b0b8c631928
Isadora Sodré <isodrechaves@gmail.com>
2021-03-15 23:27:01 -0300

    commit on 'master'

---
+++ Atividades/AT03/soma.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+int n1, n2, resultado, resultadodasoma (int resultado); 
+printf ("Digite um número qualquer: \n");
+scanf ("%d", &n1);
+printf("Digite um segundo número: \n");
+scanf ("%d", &n2);
+resultado = n1 + n2;
+printf ("O resultado da soma entre os dois números é %d", resultado);
+}
