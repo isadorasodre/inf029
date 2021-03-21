9419d5a25eb8acd0e4a09e7caf73c998196ea414
Isadora Sodré <isodrechaves@gmail.com>
2021-03-21 19:29:17 -0300

    commit on 'master'

---
+++ Atividades/AT04/ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int soma (int numero1, int numero2);
+{
+    int resultado;
+    resultado = numero1 + numero2;
+    return (resultado);
+}
+int main ()
+{
+    int n1, n2, resultado;
+    printf ("Digite o primeiro valor:\n");
+    scanf("%d", &n1);
+    printf ("Digite um segundo valor: \n");
+    scanf("%d", &n2);
+    resultado = som (n1, n2);
+    printf ("O resultado da soma é %d\n", resultado);
+    return 0;
+}
