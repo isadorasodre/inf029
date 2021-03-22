e3bc8f3499311c99dbad84f3e96dd230429dc223
Isadora Sodré <isodrechaves@gmail.com>
2021-03-22 20:18:25 -0300

    commit on 'master'

--- Atividades/AT04/ex3.c
+++ Atividades/AT04/ex3.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#include <conio.h>
-int fatorial (int n)
+int fatorial (int n);
 int main ()
 {
-        int n;
-        int resultado;
-        printf ("Digite um numero para descobrir o seu fatorial\n");
-        scanf ("%d", n);
-        resultado = fatorial(n);
-        printf ("O fatorial do numero %d é %d\n", n, resultado);
-        getch();
-        return 0;
+    int n1;
+    int resultado;
+    printf ("Digite um núemro para descobrir o seu fatorial\n");
+    scanf("%d", &n1);
+    resultado = fatorial (n1);
+    printf("O resultado é %d", resultado);
+    return 0;
 }
 int fatorial (int n)
 {
-    int resultado, n;
-    if (n == 1 || n == 0)
+    int fat;
+    int resultado;
+    if (n == 0 || n == 1);
     {
-        resultado = 1;
+        return 1;
+    }
+    if (n>1);
+    {
+        for(fat = 1; n > 1; n--);
+        fat = fat*n;
+    }
+    if (n < 0);
+    {
+    printf ("Erro\n");
+    return 0;
+        }
+        resultado = fat;
         return resultado;
-    }
-    if (n > 1)
-    {
-        resultado = n * fatorial (n-1);
-        return resultado;
-    }
-    else
-    {
-        printf("O número não pode ser calculado pois ele eh negativo\n");
-        getch();
-    }
 }
