4384165bc7f2421802cb278e066aba3e12d86827
Isadora Sodré <isodrechaves@gmail.com>
2021-04-05 00:03:04 -0300

    commit on 'master'

--- Atividades/Escola/V1.c
+++ Atividades/Escola/V1.c
@@ -2,6 +2,10 @@
 #include <stlib.h>
 #include <string.h>
 #define max_alunos = 200
+char sexo1[4] = "M";
+char sexo2[4] = "m";
+char sexo3[4] = "F";
+char sexo4[4] = "f";
 typedef struct {
     char nomealuno [200];
     char sexoaluno [5];
@@ -12,6 +16,8 @@ aluno cadalunos (max_alunos);
 void cadastroaluno();
 void cadastroprofi();
 void cadastrodisciplina();
+void listaalunos();
+void removealunos();
 int main (void)
 {
     int opçãocadastro; 
@@ -30,14 +36,14 @@ int main (void)
     
 }
 void cadastroaluno (){
-            char nomealuno [200];
-            char sexoaluno [5];
-            int diaaluno, mesaluno, anoaluno, matriculaaluno, consta;
-            long long int cpfaluno;
+        char nomealuno [200];
+        char sexoaluno [5];
+        int diaaluno, mesaluno, anoaluno, matriculaaluno, consta;
+        long long int cpfaluno;
         printf("Digite o nome do aluno: \n");
         fgets (nomealuno, sizeof(nomealuno), stdin);
         printf ("Digite o sexo do aluno: \n");
         fgets (sexoaluno, sizeof(sexoaluno), stdin);
-        if (sexoaluno != m || sexoaluno != M)
+        if (strcpm (sexoaluno, sexo1 != 0) && strcpm (sexoaluno, sexo2 != 0) && strcpm )
         
     }
