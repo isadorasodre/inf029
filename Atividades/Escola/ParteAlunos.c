2f6a4b9152569bb48b313a97aeafea5d3090de8e
Isadora Sodré <isodrechaves@gmail.com>
2021-04-11 20:04:31 -0300

    commit on 'master'

--- Atividades/Escola/ParteAlunos.c
+++ Atividades/Escola/ParteAlunos.c
@@ -3,6 +3,7 @@
 #include <conio.h>
 #include <stdint.h>
 
+//declaração de estruturas - data, professor, aluno, disciplina e aluno por disciplina
 typedef struct dados_dada{
     int dia;
     int mes; 
@@ -37,17 +38,25 @@ typedef struct aluno_disciplina{
     int codad;
 } alunodisciplina;
 
+// declaração de vetores das structs
 professor lista_professores [qtprofessores];
 aluno lista_alunos [qtalunos];
 disciplina lista_disciplinas [qtdisciplinas];
 alunodisciplina lista_matricula [alunopordisciplina];
 
+//declarações das funções que serão usadas ao longo do programa
+void alunos ();
+void listaralunos ();
+void professores ();
+void listarprofessores ();
+int conferirdata (int mes, int dia, int ano);
+int validacpf (long long int cpf);
 
+//variáveis globais
+int qtalunos, qtprofessores, qtdisciplinas = 0;
 
 int main (){
     int op;
-    cadastroaluno alunos [4];
-    int qtdalunos = 0;
     return 0;
     do{
     printf ("*****BEM-VINDO AO PROJETO ESCOLA\n");
@@ -68,12 +77,13 @@ int main (){
     printf("Você escolheu sair do programa. Obrigado pela sua participação");
     return 0;
 }
-int cadastraralunos (cadastroaluno alunos [], int qtdalunos)
-    {
-    int i = 0;
-    printf ("Digite o nome do aluno\n");
-    scanf("%c", cadastroaluno alunos[i].nomealuno);
-    }
-    }
+
+void aluno ()
+{
+    printf ("Vocês escolheu a lista de cadastro de alunos\n");
+    prinft ("Digite o nome do aluno\n");
+    setbuf(stdin, NULL);
+	fgets(vAlunos[qtdAlunos].nome, 100, stdin);
+}
 
   
