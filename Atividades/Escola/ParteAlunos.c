cd24ad90ccded1587747fd7b90bab3c120225687
Isadora Sodré <isodrechaves@gmail.com>
2021-04-11 18:37:11 -0300

    commit on 'master'

--- Atividades/Escola/ParteAlunos.c
+++ Atividades/Escola/ParteAlunos.c
@@ -2,19 +2,40 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <stdint.h>
-typedef struct {
-    char nomealuno;
-    char sexoaluno;
+
+typedef struct dados_dada{
+    int dia;
+    int mes; 
+    int ano;
+} data;
+
+typedef struct dados_aluno {
+    char nomealuno [100];
+    char sexoaluno [10];
     long long int cpfaluno;
     int matriculaaluno;
-    struct data
-	{
-		int diaaluno;
-		int mesaluno;
-		int anoaluno;
-	} dataaluno;
-} cadastroaluno;
-int cadastraralunos (cadastroaluno alunos [], int qtdalunos);
+    data datanascimento;
+} aluno;
+
+typedef struct dados_professor {
+    char nomeprof [100];
+    char sexoprof [10];
+    long long int cpfprof;
+    int matriculaaluno;
+    data datanascimento;
+} professor;
+
+typedef struct dados_disciplina {
+    char nomedisci [100];
+    int semestre;
+    char professor [100];
+    int codigo;
+} disciplina;
+
+typedef struct aluno_disciplina{
+    char nomeal [100];
+    int codad;
+} alunodisciplina;
 
 int main (){
     int op;
