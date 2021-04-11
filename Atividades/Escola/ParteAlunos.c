416ce504e750993f57dee368aa89cd2c0ff84952
Isadora Sodré <isodrechaves@gmail.com>
2021-04-11 01:10:30 -0300

    commit on 'master'

--- Atividades/Escola/ParteAlunos.c
+++ Atividades/Escola/ParteAlunos.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <stdint.h>
 typedef struct {
     char nomealuno;
     char sexoaluno;
@@ -13,14 +14,38 @@ typedef struct {
 		int anoaluno;
 	} dataaluno;
 } cadastroaluno;
-
-int cabecalhoprojeto;
 int cadastraralunos (cadastroaluno alunos [], int qtdalunos);
 
 int main (){
     int op;
-    int qtdalunos = 0;
     cadastroaluno alunos [4];
-    cabecalhoprojeto;
+    int qtdalunos = 0;
+    return 0;
+    do{
+    printf ("*****BEM-VINDO AO PROJETO ESCOLA\n");
+    printf ("ESCOLHA UMA OPÇÃO\n");
+    printf ("1 - CADASTRO DE ALUNOS\n");
+    printf ("2 - LISTAR ALUNOS\n");
+    printf ("3 - SAIR DO PROGRAMA \n");
+    scanf ("%d", &op);
+    switch (op)
+        {
+            case 1:
+            {
+                cadastraralunos ();
+            }
+            return 0;
+        }   
+    }while (op != 3);
+    printf("Você escolheu sair do programa. Obrigado pela sua participação");
     return 0;
 }
+int cadastraralunos (cadastroaluno alunos [], int qtdalunos)
+    {
+    int i = 0;
+    printf ("Digite o nome do aluno\n");
+    scanf("%c", cadastroaluno alunos[i].nomealuno);
+    }
+    }
+
+  
