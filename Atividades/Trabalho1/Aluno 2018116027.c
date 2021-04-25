6a5ad2761d175245f3b7946a4d6a8782b7213b12
Isadora Sodré <isodrechaves@gmail.com>
2021-04-25 17:38:57 -0300

    commit on 'master'

---
+++ Atividades/Trabalho1/Aluno 2018116027.c
@@ -0,0 +1,41 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Dados do Aluno -----
+//  Nome: Isadora de Sodré Chaves   
+//  email: isodrechaves@gmail.com   
+//  Matrícula: 2018116027       
+//  Semestre: (Assemestrealizada)
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 20/06/2018 - 19/08/2016
+
+// #################################################
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+int q1(char data [12])
+{
+    int datavalida = 1;
+
+    //quebrar a string data em strings sDia, sMes, sAno
+
+    //converter sDia, sMes e sAno em inteiros (ex: atoi)
+
+    //criar as variáveis iDia, iMes, iAno
+
+    //printf("%s\n", data);
+
+    if (datavalida)
+        return 1;
+    else
+        return 0;
+}
