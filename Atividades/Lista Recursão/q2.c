d9e55a1be6371704fbcd5eb84e9f49fe77db7b19
Isadora <isodrechaves@gmail.com>
2021-06-20 02:10:18 -0300

    commit on 'master'

--- Atividades/Lista Recursão/q2.c
+++ Atividades/Lista Recursão/q2.c
@@ -5,7 +5,7 @@ int fibo (int x, int y, int z);
 
 int main ()
 {
-    int x = 8;
+    int x = 200;
     int result = fibo (x, 0, 1);
     printf("O termo %d na sequencia fibonacci eh %d \n", x, result);
 }
@@ -14,10 +14,10 @@ int fibo (int x, int y, int z)
 {
     int resultado;
     if (x == 0)
-        resultado = y;
+            resultado = y;
     if (x == 1) 
-        resultado = z;
+            resultado = z;
     else
-    resultado = fibo(x-1, y, y+z);
+        resultado = fibo(x-1, y, y+z);
     return resultado; 
 }
