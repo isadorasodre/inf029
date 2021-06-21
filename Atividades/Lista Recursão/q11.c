370a7706190c497be112ce76f0fb411e3b23fea5
Isadora <isodrechaves@gmail.com>
2021-06-21 19:46:54 -0300

    commit on 'master'

--- Atividades/Lista Recursão/q11.c
+++ Atividades/Lista Recursão/q11.c
@@ -5,10 +5,10 @@ int Multip_Rec(int n1, int n2);
 
 int main ()
 {
-    int n1 = 10;
-    int n2 = 3;
-    int resultado = Multip_Rec(n1, n2)
-    ("A soma sucessiva do número, simulando uma multiplicação, é igual a %d", resultado);
+    int n1 = 120;
+    int n2 = 5;
+    int resultado = Multip_Rec(n1, n2);
+    printf ("A soma sucessiva do número, simulando uma multiplicação, é igual a %d", resultado);
 }
 
 int Multip_Rec(int n1, int n2)
@@ -17,5 +17,5 @@ int Multip_Rec(int n1, int n2)
         return 0;
     if (n2 == 1)
         return n1;
-    return n1 +  Multip_Rec(n1, n2 -1) 
+    return n1 +  Multip_Rec(n1, n2 -1); 
 }
