dd5f37779cc7610a188fb1e5fe02c508abeb667c
Isadora <isodrechaves@gmail.com>
2021-06-21 18:13:25 -0300

    commit on 'master'

---
+++ Atividades/Lista Recursão/q8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mmc (int a, int b, int c);
+
+int main ()
+{
+    int a = 10;
+    int b = 20;
+    int c = a;
+    int maxdiv = mmc(a, b, c);
+    printf("O maxim divisor comum de %d e %d eh %d", a, b, maxdiv);
+}
+
+int mmc (int a, int b, int c)
+{
+    c --;
+    if (a%c == 0 && b%c==0)
+        return c;
+    return mmc (a, b, c);
+}
