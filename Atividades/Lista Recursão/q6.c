08b6952d7a9223984374d7c964627fc74456d51f
Isadora <isodrechaves@gmail.com>
2021-06-20 22:14:44 -0300

    commit on 'master'

---
+++ Atividades/Lista Recursão/q6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int potencia (int k, int n);
+
+int main ()
+{   
+    int k, n;
+    int resultado = potencia (k, n);
+    printf("Digite um numero inteiro qualquer: \n");
+    scanf ("%d", &k);
+    printf("Digite um número no qual sera elevada a potencia: \n");
+    scanf ("%d", &n);
+    printf("Este numero elevado a esta potencia possui o resultado de %d", resultado);
+}
+
+int potencia (int k, int n)
+{
+    if (n ==0)
+        return 1;
+    if (n ==1)
+        return k;
+    if (n >1)
+        return k * potencia (k, n-1);
+}
