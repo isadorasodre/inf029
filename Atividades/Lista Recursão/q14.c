3903fdbff03279f8fd4e8604ff4aa7bd7f92650d
Isadora <isodrechaves@gmail.com>
2021-06-21 21:38:43 -0300

    commit on 'master'

--- Atividades/Lista Recursão/q14.c
+++ Atividades/Lista Recursão/q14.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pares (int a, int b);
+int parescres (int a, int b);
 
 int main ()
 {
-    int a = 24;
+    int a = 23;
     int b = 0;
-    pares (a, b);
+    parescres (a, b);
 }
 
-int pares (int a, int b)
+int parescres (int a, int b)
 {   
     if (b%2 == 0)
         printf("%d\n", b);
     if (a > b)
-        return pares (a, b+1);
+        return parescres (a, b+1);
 }
